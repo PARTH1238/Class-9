c407067a66ff83318cb5e49f86212541268474f9
PARTH1238 <123493667+PARTH1238@users.noreply.github.com>
2023-01-25 11:00:55 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,38 @@
-var box
+
 function setup() {
   createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background("orange");
-box = createSprite(100,100,50,50)
-drawSprites()
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
 }
-
-
-
 
